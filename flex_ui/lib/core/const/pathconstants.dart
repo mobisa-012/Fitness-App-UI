4fe5af4da853fdbd6fa188713d2f9e40b3915460
mobisa-012 <mobisakwamboka@gmail.com>
2022-09-09 14:52:42 +0300

    path and txt constatns

---
+++ flex_ui/lib/core/const/pathconstants.dart
@@ -0,0 +1,23 @@
+class PathConstatnts  {
+  //icons
+  static const String arrow = 'assets/icons/arrow_back.png';
+  static const String eye = 'assets/icons/eye_icon.png';
+  static const String facebook = 'assets/icons/facebook.png';
+  static const String notifications = 'assets/icons/notifications.png';
+  static const String account = 'assets/icons/person.png';
+  static const String settings = 'assets/icons/settings_icon.png';
+  static const String time = 'assets/icons/time.png';
+  static const String twitter = 'assets/icons/twitter.png';
+  static const String workout = 'assets/icons/workouts_icon.png';
+
+  //home screen images
+  static const String cardio = 'assets/images/home/cardio2.png';
+  static const String profileAvatar = 'assets/images/home/profile.png';
+  static const String yoga = 'assets/images/home/yoga2.png';
+  static const String squats = 'assets/images/home/squats2.1.png';
+
+  //onboarding screen images
+  static const String onboarding1 = 'assets/images/onboarding/onboarding1.png';
+  static const String onboarding2 = 'assets/images/onboarding/onboarding_2.png';
+  static const String onboarding3 = 'assets/images/onboarding/onboarding_.png';
+}
---
+++ flex_ui/lib/core/const/text_constants.dart
@@ -0,0 +1,35 @@
+class TextConstants {
+  //Onboardingscreen
+  static const String onboarding1Title = 'Workout Anywhere';
+  static const String onboarding2Title = 'Learn Techniques';
+  static const String onboarding3Title = 'Stay strong and healthy';
+
+  static const String onboarding1Description =
+      'Do your workouts with no equipment, at home or at the gym';
+  static const String onboarding2Description =
+      'Workout programs by professional trainers';
+  static const String onboarding3Description =
+      'Enjoy the program fully and stay healhty !!';
+
+  // Home
+  static const String topWorkouts = 'Top Workouts 💪';
+  static const String weightLossTraining = 'Weight Loss Training';
+  static const String fullBodyWorkout = 'Full Body Workout';
+  static const String time = '40 min';
+  static const String calories = '350 Cal';
+  static const String categories = 'Categories';
+  static const String yoga = 'Yoga';
+  static const String strengthTraing = 'Strength Training';
+  static const String functionalTraining = 'Functional Training';
+
+  //Analytics screen
+  static const String analytics = 'Analytics';
+  static const String caloriesTitle = 'Calories';
+  static const String activeCalories = 'Active Calories';
+  static const String workoutTime = 'Workout time';
+  static const String steps = 'Steps';
+
+  //Workout Screen
+  static const String workout = 'Workout';
+  static const String start = 'Start';
+}
