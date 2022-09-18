3136a05d4ae4a7b49789d74182d10b4678ad5991
Bina Mobisa <mobisakwamboka@gmail.com>
2022-09-18 12:17:36 +0300

    data to be shown on the workouts screen

--- flex_ui/lib/data/workout_data.dart
+++ flex_ui/lib/data/workout_data.dart
@@ -1,4 +1,25 @@
+import 'package:flex_ui/data/exercise_data.dart';
+
 class WorkoutData {
-  String? id;
-  
+  final String title;
+  final String exercises;
+  final String minutes;
+  final String currentProgress;
+  final int progress;
+  final String image;
+  final List<ExerciseData> exerciseDataList;
+
+  WorkoutData(
+      {required this.currentProgress,
+      required this.exercises,
+      required this.exerciseDataList,
+      required this.image,
+      required this.minutes,
+      required this.progress,
+      required this.title});
+
+  @override
+  String toString() {
+    return 'WorkoutData(title: $title, exercises: $exercises, minutes: $minutes, currentProgress: $currentProgress, progress: $progress, image: $image, exerciseDataList: $exerciseDataList)';
+  }
 }
