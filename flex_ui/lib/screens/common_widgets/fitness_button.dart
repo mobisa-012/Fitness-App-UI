7a0c520cf86e87f21082c4aac15be895d35c1201
Bina Mobisa <mobisakwamboka@gmail.com>
2022-09-20 14:57:12 +0300

    changes

--- flex_ui/lib/screens/common_widgets/fitness_button.dart
+++ flex_ui/lib/screens/common_widgets/fitness_button.dart
@@ -1,18 +1,16 @@
 import 'package:flex_ui/core/const/colors.dart';
 import 'package:flutter/material.dart';
 
-
 class FitnessButton extends StatelessWidget {
   final String title;
   final bool isEnabled;
   final Function() onTap;
 
-  const FitnessButton(
-      {Key? key,
+   FitnessButton(
+      {
       required this.title,
-      required this.isEnabled,
-      required this.onTap})
-      : super(key: key);
+      this.isEnabled = true,
+      required this.onTap});
 
   @override
   Widget build(BuildContext context) {
@@ -20,22 +18,23 @@ class FitnessButton extends StatelessWidget {
       width: double.infinity,
       height: 55,
       decoration: BoxDecoration(
-        color: isEnabled ? AppColors.weightLossContainerColor : AppColors.disabledColor,
+        color: isEnabled
+            ? AppColors.weightLossContainerColor
+            : AppColors.disabledColor,
         borderRadius: BorderRadius.circular(100),
       ),
       child: Material(
         color: Colors.transparent,
-        child:  InkWell(
+        child: InkWell(
           borderRadius: BorderRadius.circular(100),
           onTap: onTap,
           child: Center(
             child: Text(
               title,
               style: const TextStyle(
-                color: Colors.white,
-                fontSize: 18,
-                fontWeight: FontWeight.bold
-              ),
+                  color: Colors.white,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold),
             ),
           ),
         ),
