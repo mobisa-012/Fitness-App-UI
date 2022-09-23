49c458d1a8e3f50f6053e52e658f74e4c187e980
Bina Mobisa <mobisakwamboka@gmail.com>
2022-09-23 18:16:45 +0300

    has settings container

---
+++ flex_ui/lib/screens/common_widgets/setting_container.dart
@@ -0,0 +1,58 @@
+import 'package:flex_ui/core/const/colors.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class SettingsContainer extends StatelessWidget {
+  final bool withArrow;
+  final Widget child;
+  final Function()? onTap;
+
+  const SettingsContainer(
+      {Key? key, this.withArrow = false, required this.child, this.onTap})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      child: Container(
+        width: double.infinity,
+        height: 50,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(40),
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: AppColors.loadingBlack.withOpacity(0.12),
+              blurRadius: 5.0,
+              spreadRadius: 1.1
+            )
+          ]
+        ),
+        child: Material(
+          color: Colors.transparent,
+          child: InkWell(
+            borderRadius: BorderRadius.circular(40),
+            onTap: onTap,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: child,
+                    ),
+                    if (withArrow) const Icon(Icons.arrow_forward_ios,
+                    color: AppColors.weightLossContainerColor,
+                    size: 20,
+                    ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+      );
+  }
+}
