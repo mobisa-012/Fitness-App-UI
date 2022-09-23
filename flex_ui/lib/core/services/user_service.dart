a70d78448d1ec81111bab636a63f875d7adcd2d6
Bina Mobisa <mobisakwamboka@gmail.com>
2022-09-23 18:13:32 +0300

    enables user to edit their account

---
+++ flex_ui/lib/core/services/user_service.dart
@@ -0,0 +1,44 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flex_ui/core/extensions/exceptions.dart';
+import 'package:flex_ui/core/services/auth_service.dart';
+
+class UserService {
+  static final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  static Future<bool> editPhoto(String photoUrl) async {
+    try {
+      await _firebaseAuth.currentUser?.updatePhotoURL(photoUrl);
+      return true;
+    } catch (e) {
+      print(e);
+      return false;
+    }
+  }
+
+  static Future<bool> changeUserData(
+      {required String displayName, required String email}) async {
+    try {
+      await _firebaseAuth.currentUser?.updateDisplayName(displayName);
+      await _firebaseAuth.currentUser?.updateEmail(email);
+      return true;
+    } catch (e) {
+      print(e);
+      throw Exception(e);
+    }
+  }
+
+  static Future<bool> changePassword({required String newPassword}) async {
+    try {
+      await _firebaseAuth.currentUser?.updatePassword(newPassword);
+      return true;
+    } on FirebaseAuthException catch (e) {
+      throw CustomFirebaseException(getExceptionMessage(e));
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
+
+  static Future<void> signOut() async {
+    await _firebaseAuth.signOut();
+  }
+}
