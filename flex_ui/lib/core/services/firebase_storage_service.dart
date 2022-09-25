a2a51c78b7da53936c3ff852d2f24647e4adb4b0
mobisa-012 <mobisakwamboka@gmail.com>
2022-09-25 12:46:54 +0300

    storage method

--- flex_ui/lib/core/services/firebase_storage_service.dart
+++ flex_ui/lib/core/services/firebase_storage_service.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flex_ui/core/services/user_service.dart';
 
 
